b5e1bec19e1461e77d646f7cd97facb51598ad13
Stoltz-Limin <s_stoltz-limin@tss-hr420-dm11.local>
2019-01-24 17:57:03 -0500

    attempts to fix spelling errors

--- js/main.js
+++ js/main.js
@@ -2,18 +2,50 @@
   // create a component first
 const HomePageComponent = {
   template: "<h2>This is the home page</h2>"
-},
+};
 
 const UsersPageComponent = {
-  template: "<h2>Thsi is the users page</h2>"
-},
+  props: ['id'], //this.id
+  template: "#userList",
+
+//encapsulate or isolate all component data
+  data: function(){
+    return {
+      users: []
+    }
+  },
+
+  created: function() {
+    console.log('user component created!');
+
+    //take the query perameter from the route and pass it to the fetchUserData methods
+
+    this.fetchUserData(this.id);
+  },
+
+  methods: {
+    fetchUserData(user) {
+      debugger;
+
+      let url = `./includes/index.php?users=${this.id}`;
+
+      fetch(url)
+      .then(res => res.json())
+      .then(data => this.users = data)
+      .catch(function(error) {
+        console.error(error);
+      });
+    }
+  }
+};
+
 const ContactPageComponent = {
-  template: "<h2>Thsi is the contact page</h2>"
-},
+  template: "<h2>This is the contact page</h2>"
+};
 
 const ErrorPageComponent = {
   template: "<h2>Page not found! Try again...</h2>"
-},
+};
 //routes are the path you're taking through the application
 //www.google.ca is a route; it's the route to the home page
 // www.sumpinsumpin/contact is the route to the contact page, etc.
@@ -21,9 +53,8 @@ const ErrorPageComponent = {
 // and that component gets rendered in the <router-view> element
 const routes = [
   { path: '/', name: 'home', component: HomePageComponent },
-  { path: '/contact', name: 'contact', component: ContactPageComponent },
-  {path: '/users', name: 'users', component: UsersPageComponent },
-  { path: '/*', name: 'error', component: ErrorPageComonent}
+  {path: '/user/:id', name: 'users', component: UsersPageComponent, props: true },
+  { path: '/contact', name: 'contact', component: ContactPageComponent }
 ]
 
 const router = new VueRouter({
@@ -37,10 +68,17 @@ const router = new VueRouter({
     data: {
       testmessage: "sup"
     },
+    created: function() {
+      console.log('parent is live');
+    },
 
     methods: {
-      calledOnParent(){
-        console.log("This method lives in the main VM and was called from a component");
+      logParent(){
+        console.log("from the parent". message);
+      },
+
+      logMainMessage(message) {
+        console.log("called from inside a child, lives in the parent", message);
       }
     },
 
@@ -49,10 +87,10 @@ created: function() {
 },
 
     components: {
-      'HomePageComponent' : HomePageComponent
-      'UsersPageComponentcomponent' : UsersPageComponent
-      'ContactPageComponentcomponent' : ContactPageComponent
-    }
+      'HomePageComponent' : HomePageComponent,
+      'UsersPageComponent' : UsersPageComponent,
+      'ContactPageComponent' : ContactPageComponent
+    },
 
     router: router
   });
